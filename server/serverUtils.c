8ef938912bf1db5a845a82ff4d8703b70391f9fe
Pedro <pdihax@gmail.com>
2023-06-09 17:03:14 -0300

    Change master socket generation because the last one didn't comply with c11

--- server/serverUtils.c
+++ server/serverUtils.c
@@ -1,58 +1,49 @@
 #ifndef SERVER_UTILS
 #define SERVER_UTILS
+#include <asm-generic/socket.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <netdb.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 #include <unistd.h>
 #include <errno.h>
-#include "../shared/util.h"
 #include "../shared/logger.h"
 #include "serverUtils.h"
-#define MAX_BUFFER 512
-
-static char addrBuffer[MAX_BUFFER];
 
 int createTCPSocketServer(const char* service) {
-    //Construct the server address structure
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC; //For IPv4 and IPv6
-    hints.ai_flags = AI_PASSIVE; // Because is a server socket
-    hints.ai_socktype = SOCK_STREAM; // TCP Stream
-    hints.ai_protocol = IPPROTO_TCP; // TCP
+    unsigned int port = atoi(service);
+    struct sockaddr_in addr;    
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    struct addrinfo* addrArray;
-    int addrReturnValue = getaddrinfo(NULL, service, &hints, &addrArray);
-    if (addrReturnValue == -1) {
-        log(FATAL, "Error getting address info: %s", strerror(errno));        
+    const int serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket < 0) {
+        log(ERROR, "[serverUtils][createTCPSocketServer] Unable to create socket");
+        return -1; 
     }
 
-    int servSocket = -1;
-    for (struct addrinfo * addr = addrArray; addr != NULL && servSocket == -1; addr = addr->ai_next) {
-        servSocket = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
-        if (servSocket < 0) {
-            log(DEBUG, "Can't create socket on %s: %s", printAddressPort(addr, addrBuffer), strerror(errno));
-            continue;
-        }
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+        
 
-        //Bind to all the address and set socket to listen
-        if ((bind(servSocket, addr->ai_addr, addr->ai_addrlen) == 0) && (listen(servSocket, MAX_PENDING) == 0)) {
-            struct sockaddr_storage localAddr;
-            socklen_t addrSize = sizeof(localAddr);
-            if (getsockname(servSocket, (struct sockaddr *) &localAddr, &addrSize) >= 0) {
-                printSocketAddress((struct sockaddr *) &localAddr, addrBuffer);
-                log(INFO, "Binding to %s", addrBuffer);
-            }
-        } else {
-            log(DEBUG, "Can't bind %s", strerror(errno));
-            close(servSocket);
-            servSocket = -1;
-        }
+    if (bind(serverSocket, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+        log(ERROR, "[serverUtils][createTCPSocketServer] Unable to bind socket %d", serverSocket);
+        close(serverSocket);
+        return -1;
     }
-    freeaddrinfo(addrArray);
-    return servSocket;
+    log(DEBUG, "[serverUtils][createTCPSocketServer] Socket %d bound to port %d", serverSocket, port);
+
+    if (listen(serverSocket, port)) {
+        log(ERROR, "[serverUtils][createTCPSocketServer] Unable to listen to port %d with socket %d", port, serverSocket);
+        close(serverSocket);
+        return -1;
+    }
+    
+    log(DEBUG, "[serverUtils][createTCPSocketServer] Socket %d listening", serverSocket);
+    
+    return serverSocket;
 }
 
 #endif 
