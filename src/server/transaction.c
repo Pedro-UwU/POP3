bdb9a878ff870a3a8ee86f7f8fbb60f40044d8d0
Martín E. Zahnd <mzahnd@itba.edu.ar>
2023-06-19 09:26:46 -0300

    Remove mock RETR and use real mail
    * Also moves `is_sending` case to `send_data`
    
    * Fix bug when listing last email (by index) using LIST

--- src/server/transaction.c
+++ src/server/transaction.c
@@ -12,12 +12,15 @@
 #include <utils/logger.h>
 #include <utils/maildir.h> // maildir_*
 
-static void handle_request(client_data *data, char msg[MAX_RSP_LEN]);
+static unsigned send_data(struct selector_key *key);
+static void handle_request(struct selector_key *key, char msg[MAX_RSP_LEN]);
+
 static void cmd_stat(client_data *data, char msg[MAX_RSP_LEN]);
 static void cmd_list(client_data *data, char msg[MAX_RSP_LEN]);
+static void cmd_retr(struct selector_key *key, char msg[MAX_RSP_LEN]);
 static int get_new_mails(client_data *data, maildir_mails_t **mails, unsigned long *size);
+
 static char *get_next_arg(client_data *data, char buf[MAX_ARG_LEN]);
-static void mock_read_file(struct selector_key* key);
 
 void init_trans(const unsigned int state, struct selector_key *key)
 {
@@ -29,6 +32,7 @@ void finish_trans(const unsigned int state, struct selector_key *key)
 {
         client_data *data = GET_DATA(key);
         maildir_close(data->parser.trans_parser.maildir);
+        data->parser.trans_parser.maildir = NULL;
 }
 
 unsigned trans_read(struct selector_key *key)
@@ -73,47 +77,28 @@ unsigned trans_process(struct selector_key *key)
 {
         client_data *data = GET_DATA(key);
 
-        if (data->is_sending == true) {
-                size_t bytes_to_send = 0;
-                uint8_t *bytes = buffer_read_ptr(&data->write_buffer_client, &bytes_to_send);
-                size_t can_send = bytes_to_send > MAX_BYTES_SEND ? MAX_BYTES_SEND : bytes_to_send;
-                ssize_t sent = send(key->fd, bytes, can_send, 0);
-                if (sent < 0) {
-                        data->err_code = UNKNOWN_ERROR;
-                        return ERROR_POP3;
-                }
-                buffer_read_adv(&data->write_buffer_client, sent);
-                if ((size_t)sent == bytes_to_send) { // Everything was sent
-                        if (data->sending_file == true) {
-                                load_more_bytes(key);
-                                // SET INTEREST OF FILE SELECTOR TO READ AND SELF TO NOOP
-                                //
-                        } else {
-                                data->is_sending = false;
-                                if (buffer_can_read(&data->read_buffer_client) == false) {
-                                        selector_set_interest_key(key, OP_READ);
-                                }
-                                return data->next_state;
-                        }
-                }
-                return TRANSACTION;
-        }
+        buffer *output_buffer = &data->write_buffer_client;
+        buffer *input_buffer = &data->read_buffer_client;
 
-        if (buffer_can_read(&data->read_buffer_client) == false) {
+        if (data->is_sending == true)
+                return send_data(key);
+
+        if (buffer_can_read(input_buffer) == false) {
                 selector_set_interest_key(key, OP_READ);
                 return TRANSACTION;
         }
 
         trans_parser_t *parser = &data->parser.trans_parser;
-        trans_parse(key, parser, &data->read_buffer_client);
+        trans_parse(key, parser, input_buffer);
         if (parser->ended == false) {
                 selector_set_interest_key(key, OP_READ);
                 return TRANSACTION;
         }
-        // Hande request
+
+        // Handle request
         char msg[MAX_RSP_LEN];
 
-        handle_request(data, msg);
+        handle_request(key, msg);
 
         ssize_t sent_bytes = write_msg(key, msg);
         if (sent_bytes < 0) {
@@ -121,28 +106,72 @@ unsigned trans_process(struct selector_key *key)
                 return ERROR_POP3;
         }
 
-        // TODO from here
-        mock_read_file(key);
-        if (buffer_can_read(&data->write_buffer_client) == false) {
-                log(ERROR, "Nothing to read after handling the request in TRANSACTION. Socket %d",
-                    key->fd);
-                init_trans_parser(parser, (const char *)data->user);
-                return TRANSACTION; // TODO Change to error
+        buffer_read_adv(output_buffer, sent_bytes);
+        if (buffer_can_read(output_buffer) == true) { // Things left to send
+                data->is_sending = true;
+                goto finally;
         }
-        init_trans_parser(parser, (const char *)data->user);
-        data->is_sending = true;
+
+        if (buffer_can_read(input_buffer) == true) { // No more things to read. (PIPELINING)
+                selector_set_interest_key(key, OP_WRITE);
+        } else {
+                selector_set_interest_key(key, OP_READ);
+        }
+
+finally:
+        if (data->next_state == TRANSACTION)
+                init_trans_parser(parser, (const char *)data->user);
+
+        return data->next_state;
+}
+
+static unsigned send_data(struct selector_key *key)
+{
+        client_data *data = GET_DATA(key);
+
+        buffer *output_buffer = &data->write_buffer_client;
+        buffer *input_buffer = &data->read_buffer_client;
+
+        size_t bytes_to_send = 0;
+        uint8_t *bytes = buffer_read_ptr(output_buffer, &bytes_to_send);
+        size_t can_send = bytes_to_send > MAX_BYTES_SEND ? MAX_BYTES_SEND : bytes_to_send;
+
+        ssize_t sent = send(key->fd, bytes, can_send, 0);
+        if (sent < 0) {
+                data->err_code = UNKNOWN_ERROR;
+                return ERROR_POP3;
+        }
+
+        buffer_read_adv(output_buffer, sent);
+        if ((size_t)sent == bytes_to_send) { // Everything was sent
+                if (data->sending_file == true) {
+                        // SET INTEREST OF FILE SELECTOR TO READ AND SELF TO NOOP
+                        load_more_bytes(key);
+                } else {
+                        data->is_sending = false;
+                        if (buffer_can_read(input_buffer) == false)
+                                selector_set_interest_key(key, OP_READ);
+
+                        return data->next_state;
+                }
+        }
+
         return TRANSACTION;
 }
 
-static void handle_request(client_data *data, char msg[MAX_RSP_LEN])
+static void handle_request(struct selector_key *key, char msg[MAX_RSP_LEN])
 {
-        log(DEBUG, "HANDLING CMD: %s - ARGS: %s", data->parser.trans_parser.cmd,
-            data->parser.trans_parser.arg);
+        client_data *data = GET_DATA(key);
+        char *cmd = data->parser.trans_parser.cmd;
 
-        if (strcmp(data->parser.trans_parser.cmd, "STAT") == 0) {
+        log(DEBUG, "[TRANSACTION] CMD: %s | ARGS: %s", cmd, data->parser.trans_parser.arg);
+
+        if (strcmp(cmd, "STAT") == 0) {
                 cmd_stat(data, msg);
-        } else if (strcmp(data->parser.trans_parser.cmd, "LIST") == 0) {
+        } else if (strcmp(cmd, "LIST") == 0) {
                 cmd_list(data, msg);
+        } else if (strcmp(cmd, "RETR") == 0) {
+                cmd_retr(key, msg);
         } else {
                 sprintf(msg, "-ERR invalid command\r\n");
         }
@@ -193,15 +222,57 @@ static void cmd_list(client_data *data, char msg[MAX_RSP_LEN])
                 return;
         }
 
-        sprintf(msg, "+OK %d %d\r\n", n, mails->mails[n].size);
+        sprintf(msg, "+OK %d %d\r\n", n, mails->mails[n - 1].size);
+}
+
+static void cmd_retr(struct selector_key *key, char msg[MAX_RSP_LEN])
+{
+        client_data *data = GET_DATA(key);
+
+        maildir_mails_t *mails = NULL;
+        unsigned long size = 0;
+        if (get_new_mails(data, &mails, &size) != 0) {
+                log(ERROR, "NULL mails");
+                sprintf(msg, "-ERR server error\r\n");
+                return;
+        }
+
+        char buf[MAX_ARG_LEN] = { 0 };
+
+        char *arg = get_next_arg(data, buf);
+        int n = atoi(arg);
+
+        if (n == 0) {
+                sprintf(msg, "-ERR could not understand argument, %d messages in maildrop\r\n",
+                        mails->len);
+                return;
+        } else if (n < 0 || (unsigned)n > mails->len) {
+                sprintf(msg, "-ERR no such message, only %d messages in maildrop\r\n", mails->len);
+                return;
+        }
+
+        sprintf(msg, "+OK %d octects\r\n", mails->mails[n - 1].size);
+
+        data->sending_file = true;
+
+        strcpy(data->fr_data.file_path, mails->mails[n - 1].path);
+
+        data->fr_data.is_reading_file = &data->sending_file;
+        data->fr_data.output_buffer = &data->write_buffer_client;
+        data->fr_data.client_fd = key->fd;
+        data->is_sending = true;
+
+        init_file_reader(key, &(data->fr_data));
+
+        log(DEBUG, "Changing client to NOOP");
+        selector_set_interest_key(key, OP_NOOP);
 }
 
 static int get_new_mails(client_data *data, maildir_mails_t **mails, unsigned long *size)
 {
         *mails = maildir_list_new(data->parser.trans_parser.maildir);
-        if (*mails == NULL) {
+        if (*mails == NULL)
                 return 1;
-        }
 
         *size = 0;
         for (unsigned i = 0; i < (*mails)->len; i++) {
@@ -217,13 +288,16 @@ static char *get_next_arg(client_data *data, char buf[MAX_ARG_LEN])
 
         int i = 0;
         while (data->parser.trans_parser.arg_read > sep) {
-                for (; i < 1 + (MAX_ARG_LEN * MAX_ARGS); i++) {
-                        if (data->parser.trans_parser.arg[i] == ' ' ||
-                            data->parser.trans_parser.arg[i] == '\0') {
+                while (i < 1 + (MAX_ARG_LEN * MAX_ARGS)) {
+                        if (data->parser.trans_parser.arg[i] == ' ') {
+                                i++; // Point to char after separator
                                 sep++;
-                                i++;
+                                break;
+                        } else if (data->parser.trans_parser.arg[i] == '\0') {
+                                sep++;
                                 break;
                         }
+                        i++;
                 }
         }
 
@@ -240,25 +314,4 @@ static char *get_next_arg(client_data *data, char buf[MAX_ARG_LEN])
         data->parser.trans_parser.arg_read++;
 
         return buf;
-}
-
-static void mock_read_file(struct selector_key* key) {
-    client_data* data = GET_DATA(key);
-    char * cmd = data->parser.trans_parser.cmd;
-    if (strcmp(cmd, "RETR") == 0) {
-        data->sending_file = true;
-
-        strcpy(data->fr_data.file_path, "/home/pedro/Documents/temp/random_file");
-        data->fr_data.is_reading_file = &data->sending_file;
-        data->fr_data.output_buffer = &data->write_buffer_client;
-        data->fr_data.client_fd = key->fd;
-        data->is_sending = true;
-        data->next_state = TRANSACTION;
-        init_file_reader(key, &(data->fr_data)) ;
-        log(DEBUG, "Changing client to NOOP");
-        selector_set_interest_key(key, OP_NOOP);
-    } else {
-        log(DEBUG ,"SADAASDASDASDS");
-        return;
-    }
 }
