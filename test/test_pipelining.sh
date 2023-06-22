595479fd7fcd6939c6573786d230b87925c6d33b
Martín E. Zahnd <mzahnd@itba.edu.ar>
2023-06-22 07:11:04 -0300

    Improve test. Improve bash code quality

--- test/test_pipelining.sh
+++ test/test_pipelining.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+#
+# shellcheck disable=2155
+readonly SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+pushd "$SCRIPT_DIR" &> /dev/null || exit 1
 
 ip="127.0.0.1"
 port=60711
-# command="USER PEDRO\r\nUSER USER1\r\nPASS 123A\r\nUSER USER1\r\nPASS 12345\r\nQUIT\r\n"  # Replace with the command you want to send
-command="USER USER1\r\nPASS 12345\r\RETR\r\n"
+pop3_command="USER USER1\r\nPASS 12345\r\nLIST\r\nSTAT\r\nQUIT\r\n"
+
+exit_val=0
 
 # Send the command through netcat
-echo -e "$command" | nc -C $ip $port
-
-if [ $? -eq 0 ]; then
+if echo -en "$pop3_command" | nc -C $ip $port ; then
     echo "Command sent to $ip:$port"
 else
     echo "Error connecting to $ip:$port"
+    exit_val=1
 fi
 
+popd &> /dev/null || exit 1 # $SCRIPT_DIR
+exit $exit_val
