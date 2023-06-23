928c88083b097ed7fe2d7e3e40aa33a07a261d69
Martín E. Zahnd <mzahnd@itba.edu.ar>
2023-06-23 09:39:13 -0300

    Fix script not running on fresh clone
    * Results directory was not being created
    
    * Add variables for easily modifying the test settings
    
    * Add error handling when nc fails
    
    * Fix unquoted variables

--- test/test_500_users.sh
+++ test/test_500_users.sh
@@ -22,10 +22,20 @@
 # SOFTWARE.
 #
 
+# shellcheck disable=2155
+readonly SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+pushd "$SCRIPT_DIR" &> /dev/null || exit 1
+
+dir_results="results/"
+
+ip="127.0.0.1"       # Replace with the desired IP address
+port=60401           # Replace with the desired port number
+timeout_duration=5   # Replace with the desired timeout duration in seconds
+
 # Function to generate ADD_USER commands
 generate_add_user_commands() {
     for ((i=1; i<=500; i++)); do
-        echo "ADD_USER user$i 1234"
+        echo "ADD_USER user$i 1234 "
     done
 }
 
@@ -33,8 +43,15 @@ generate_add_user_commands() {
 commands="LOGIN admin admin\r\n$(generate_add_user_commands)\r\n"
 
 # Execute the commands using nc
-echo -e "$commands" | nc -w 5 -C 127.0.0.1 60401
+if ! echo -en "$commands" | nc -w "$timeout_duration" -C "$ip" "$port" ; then
+    echo "Error while trying to connect (nc)"
+    exit 1
+fi
+
+mkdir -p "$dir_results"
 
 for ((i=1; i<=500; i++)); do
-    curl --url 'pop3://127.0.0.1:60711/' -u "user$i:1234" > /dev/null 2> results/user$i.txt &
+    curl --url 'pop3://127.0.0.1:60711/' -u "user$i:1234" > /dev/null 2> "$dir_results"/"user$i.txt" &
 done
+
+echo "See curl output in: $SCRIPT_DIR/$dir_results"
